2e62009ee8d603a73121757b51c2ee29a8ba43f5
Neha Kennard <nnayak@cs.umass.edu>
2021-10-14 02:00:56 -0400

    add run script

---
+++ combined_alignment/run_main_2tbin.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+cd /mnt/nfs/scratch1/nnayak/peer-review-discourse-dataset/
+conda deactivate
+source ve/bin/activate
+module load python3/3.9.0-2010
+
+cd combined_alignment
+
+python train.py -i torchtext_input_data_posneg_1_sample_1.0/ -t bin -r 2t
